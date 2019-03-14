01e5d9090364731fd7d942b111cdc0fe945b6e20
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2019-03-14 16:52:06 +0600

    second commit

--- common/Handlers.go
+++ common/Handlers.go
@@ -1,1 +1,127 @@
 package common
+
+import (
+	"awesomeProject/login-reg-cookie-demo/helpers"
+	"awesomeProject/login-reg-cookie-demo/repos"
+	"fmt"
+	"github.com/gorilla/securecookie"
+	"net/http"
+)
+
+var cookieHandler = securecookie.New(
+	securecookie.GenerateRandomKey(64),
+	securecookie.GenerateRandomKey(32))
+
+// Handlers
+
+// for GET
+func LoginPageHandler(w http.ResponseWriter, r *http.Request) {
+	var body, _ = helpers.LoadFile("templates/login.html")
+	fmt.Fprintf(w, body)
+}
+
+// for POST
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	pass := r.FormValue("password")
+
+	redirectTarget := "/"
+
+	if !helpers.IsEmpty(name) && !helpers.IsEmpty(pass) {
+		// Database check for user data
+		_userIsValid := repos.UserIsValid(name, pass)
+
+		if _userIsValid {
+			SetCookie(name, w)
+			redirectTarget += "index"
+		} else {
+			redirectTarget += "register"
+		}
+	}
+
+	http.Redirect(w, r, redirectTarget, 302)
+}
+
+// for GET
+func RegisterPageHandler(w http.ResponseWriter, r *http.Request) {
+	var body, _ = helpers.LoadFile("templates/register.html")
+	fmt.Fprintf(w, body)
+}
+
+// for POST
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	uName := r.FormValue("username")
+	email := r.FormValue("email")
+	pwd := r.FormValue("password")
+	confirmPwd := r.FormValue("confirmPassword")
+
+	_uName, _email, _pwd, _confirmPwd := false, false, false, false
+	_uName = !helpers.IsEmpty(uName)
+	_email = !helpers.IsEmpty(email)
+	_pwd = !helpers.IsEmpty(pwd)
+	_confirmPwd = !helpers.IsEmpty(confirmPwd)
+
+	if _uName && _email && _pwd && _confirmPwd {
+		fmt.Fprintln(w, "Username for Register : ", uName)
+		fmt.Fprintln(w, "Email for Register : ", email)
+		fmt.Fprintln(w, "Password for Register : ", pwd)
+		fmt.Fprintln(w, "ConfirmPassword for Register : ", confirmPwd)
+	} else {
+		fmt.Fprintln(w, "This fields can not be blank!")
+	}
+}
+
+// for GET
+func IndexPageHandler(w http.ResponseWriter, r *http.Request) {
+	userName := GetUserName(r)
+	if !helpers.IsEmpty(userName) {
+		var indexBody, _ = helpers.LoadFile("templates/index.html")
+		fmt.Fprintf(w, indexBody, userName)
+	} else {
+		http.Redirect(w, r, "/", 302)
+	}
+}
+
+// for POST
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+	ClearCookie(w)
+	http.Redirect(w, r, "/", 302)
+}
+
+// Cookie
+
+func SetCookie(userName string, w http.ResponseWriter) {
+	value := map[string]string{
+		"name": userName,
+	}
+	if encoded, err := cookieHandler.Encode("cookie", value); err == nil {
+		cookie := &http.Cookie{
+			Name:  "cookie",
+			Value: encoded,
+			Path:  "/",
+		}
+		http.SetCookie(w, cookie)
+	}
+}
+
+func ClearCookie(w http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:   "cookie",
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
+	}
+	http.SetCookie(w, cookie)
+}
+
+func GetUserName(r *http.Request) (userName string) {
+	if cookie, err := r.Cookie("cookie"); err == nil {
+		cookieValue := make(map[string]string)
+		if err = cookieHandler.Decode("cookie", cookie.Value, &cookieValue); err == nil {
+			userName = cookieValue["name"]
+		}
+	}
+	return userName
+}
--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"awesomeProject/login-reg-cookie-demo/common"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+var myRouter = mux.NewRouter()
+
 func main() {
 
+	myRouter.HandleFunc("/", common.LoginPageHandler).Methods("GET")
+
+	myRouter.HandleFunc("/index", common.IndexPageHandler).Methods("GET")
+	myRouter.HandleFunc("/login", common.LoginHandler).Methods("POST")
+
+	myRouter.HandleFunc("/register", common.RegisterPageHandler).Methods("GET")
+	myRouter.HandleFunc("/register", common.RegisterHandler).Methods("POST")
+
+	myRouter.HandleFunc("/logout", common.LogoutHandler).Methods("POST")
+
+	//http.Handle("/", myRouter)
+	http.ListenAndServe(":8010", myRouter)
 }
